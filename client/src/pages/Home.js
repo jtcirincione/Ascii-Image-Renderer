9e0a6d73b72fa70a163a304d5dad427ec3eec77e
John Cirincione <jtcirinc@ncsu.edu>
2024-06-29 21:53:02 -0400

    Finished ASCII vid generation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,28 @@
 import { useEffect, useRef, useState } from "react";
 import socketIOClient from 'socket.io-client';
+import io from 'socket.io-client'
 
 
 function Home() {
 
     // get data from backend
     const [data, setData] = useState(null);
+    const[isPlaying, setIsPlaying] = useState(false)
     const videoRef = useRef(null)
     const canvasRef = useRef(null)
-    const ENDPOINT = "http://localhost:80";
-    let socket = socketIOClient(ENDPOINT)
+    const ENDPOINT = "http://localhost:3001";
+    let socket = io(ENDPOINT)
     
     const capture_frame = () => {
-        let video = videoRef.current;
-        let canvas = canvasRef.current;
+        const video = videoRef.current;
+        const canvas = canvasRef.current;
         if (canvas) {
             const context = canvas.getContext("2d");
             
-            if (video && context) {
+            if (video && context && isPlaying == false) {
                 context.drawImage(video , 0, 0);
-                const image_data = canvas.toDataURL("image/png");
-                socket.emit("video_frame", image_data);
+                const image_data = canvas.toDataURL("image/jpeg", 0.1);                
+                socket.emit("vid-frame", image_data);
             }
         }
     }
@@ -30,19 +32,30 @@ function Home() {
         navigator.mediaDevices.getUserMedia({ video: true })
             .then(stream => {
                 const video = videoRef.current;
-                if (video) {
+                if (video && isPlaying == false) {
                     video.srcObject = stream;
                     video.play().catch(error => console.log(error));
+                    setIsPlaying(true)
                 }
             })
             .catch(error => console.log("Error accessing video stream:", error));
 
         // Set up the interval to capture frames
-        const intervalId = setInterval(capture_frame, 100);
+        const intervalId = setInterval(capture_frame, 75);
 
         // Clean up the interval on component unmount
-        return () => clearInterval(intervalId);
+        return () => {
+            clearInterval(intervalId);
+            const video = videoRef.current;
+            if (video) {
+                video.pause();
+                setIsPlaying(false)
+            }
+        }
     }, []);
+
+
+    
 
 
     useEffect(() => {
@@ -52,18 +65,18 @@ function Home() {
         });
 
         // Clean up the socket connection on component unmount
-        return () => socket.disconnect();
     }, [socket]);
 
 
     return (
         <div>
 
-            <video ref={videoRef} className="" width={640} height={480} autoPlay></video>
+            <video ref={videoRef} className="" width={640} height={480}></video>
 
-            <canvas ref={canvasRef} width={640} height={480}>
+            <canvas className="hidden" ref={canvasRef} width={640} height={480}>
 
             </canvas>
+            {data && <pre>{data}</pre>}
         </div>
 
 
--- server/src/app.py
+++ server/src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, Response
 from flask_socketio import SocketIO, emit
 import numpy as np, base64
+import cv2
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,22 +10,46 @@ def hello():
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+ascii_chars = "@$%#BWXT*+=-:.          "
+
+table = [ascii_chars[(i * (len(ascii_chars) - 1)) // 255] for i in range(256)]
 
 
 @socketio.on('vid-frame')
 def process_frame(frame_data):
     image_data = base64.b64decode(frame_data.split(",")[1])
-    print(image_data)
+    np_arr = np.frombuffer(image_data, np.uint8)
+    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    # print("received!")
+    ascii_image = frame_to_ascii(frame)
+    
+    # Send the ASCII image back to the client
+    emit('ascii_image', {'image': ascii_image})
 
 
-# def frame_to_ascii(frame):
-#     ascii_chars = " .:-=+*#%$@"
-#     ascii_image = ""
-#     for row in gray_frame:
-#             for pixel_value in row:
-#                 ascii_image += ascii_chars[(pixel_value * (len(ascii_chars) - 1)) // 255]
-#             ascii_image += '\n'
+
+def frame_to_ascii(frame):
+
+    height, width = frame.shape[:2]
+    aspect_ratio = height / width
+    ## play with these numbers to change resolution/size of ascii image
+    new_width = 200
+    new_height = int(aspect_ratio * new_width * .5)
+    resized_frame = cv2.resize(frame, (new_width, new_height))
+    
+    # Convert to grayscale
+    gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+    ascii_image = ""
+    for row in gray_frame:
+            for pixel_value in row:
+                idx = pixel_value
+                if idx < 0: idx = 0
+                ascii_image += table[idx]
+            ascii_image += '\n'
+
+    # print(f"Ascii image: {ascii_image}")
+    return ascii_image
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3001)
+    socketio.run(app, host='0.0.0.0',  port=3001, debug=True, allow_unsafe_werkzeug=True)
